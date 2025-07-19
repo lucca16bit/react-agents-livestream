18baeb214d3bea8799ed990ec2cb4f2e50272412
lucas <lucaston2323@gmail.com>
2025-07-18 23:59:37 -0300

    chore: deploy config

--- src/utils/format-date.utils.ts
+++ src/utils/format-date.utils.ts
@@ -1,8 +1,8 @@
 import lib from 'dayjs';
-import 'dayjs/locale/pt-BR';
+import 'dayjs/locale/pt-br';
 import relativeTime from 'dayjs/plugin/relativeTime';
 
-lib.locale('pt-BR');
+lib.locale('pt-br');
 lib.extend(relativeTime);
 
 export const dayjs = lib;
